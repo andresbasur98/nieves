85d3b15cbd153cf1c9dd4e6e3d48dfbc2509856f
Andres Basurto <andresbasur98@gmail.com>
2022-10-16 19:53:49 +0200

    Main sin social icons ni menu

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useRef, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
 import useSound from 'use-sound'
 
 
@@ -45,45 +45,58 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
+
       <Container>
+      <Luz />
+      <Luz top={'69%'} left={'70%'} background={'#82d4fb'}/>
         <Title
           style={{borderTopLeftRadius: '999px'}}
           animate={!playing ? {opacity: 1} : {opacity: 0}} 
-          transition={!playing ? { type: "tween", delay: 0.3, duration: 0.8 }: {delay: 0}} 
+          transition={!playing ? { type: "tween", delay: 0.5, duration: 0.8 }: {delay: 0}} 
         >
           JUAN NIEVES
         </Title> 
         <DiscContainer 
           onClick={handleClick}
+          animate={{
+            rotate: playing ? 360 : 0,
+          }}
+          transition={{
+            duration: playing ? 4 : 0.3,
+            delay: 0.3,
+            ease: "linear",
+            times: [0, 1],
+            repeat: playing ? Infinity: null,
+          }}
         >
             <Disc 
-              style={{borderTopLeftRadius: '999px'}}
+              style={{borderTopLeftRadius: '999px', width: '50%', height: '50%'}}
               animate={!playing ? {y: -50, x: -10} : {y:0}} 
               transition={{ type: "tween" }} 
             >
-              <Image src="/assets/images/dj-disc1.jpg" layout='fill' style={{borderTopLeftRadius: '999px', objectFit:'cover'}}/>
+              <Image src="/assets/images/dj-disc1.jpg" layout='fill' objectFit='cover' style={{borderTopLeftRadius: '999px', objectFit:'cover'}}/>
             </Disc>
             <Disc 
-              style={{borderTopRightRadius: '999px'}}
+              style={{borderTopRightRadius: '999px', width: '50%', height: '50%'}}
               animate={!playing ? {y: -50, x: 10} : {y:0}}  
               transition={{ type: "tween" }} 
             >
-              <Image src="/assets/images/dj-disc2.jpg" layout='fill' style={{borderTopRightRadius: '999px', objectFit:'cover'}} />
+              <Image src="/assets/images/dj-disc3.jpg" layout='fill' objectFit='cover' style={{borderTopRightRadius: '999px', objectFit:'cover'}} />
             </Disc>
 
             <Disc 
-              style={{borderBottomLeftRadius: '999px'}}
+              style={{borderBottomLeftRadius: '999px', width: '50%', height: '50%'}}
               animate={!playing ? {y: 50, x: -10} : {y:0}} 
               transition={{ type: "tween" }} 
             >
-              <Image src="/assets/images/dj-disc3.jpg" layout='fill' style={{borderBottomLeftRadius: '999px', objectFit:'cover'}}/>
+              <Image src="/assets/images/dj-disc2.jpg" layout='fill' objectFit='cover' style={{borderBottomLeftRadius: '999px', objectFit:'cover'}}/>
             </Disc>
             <Disc 
-              style={{borderBottomRightRadius: '999px'}}
+              style={{borderBottomRightRadius: '999px', width: '50%', height: '50%'}}
               animate={!playing ? {y: 50, x: 10} : {y:0}} 
               transition={{ type: "tween" }} 
             >
-              <Image src="/assets/images/dj-disc4.jpg" layout='fill' style={{borderBottomRightRadius: '999px', objectFit:'cover'}}/>
+              <Image src="/assets/images/dj-disc4.jpg" layout='fill' objectFit='cover' style={{borderBottomRightRadius: '999px', objectFit:'cover'}}/>
             </Disc>
         </DiscContainer>
         {
@@ -114,13 +127,22 @@ const Title = styled(motion.h1)`
     position: absolute;
     font-family: 'Orbitron', sans-serif;
     font-weight: 900;
-    font-size: 5rem;
+    font-size: 6rem;
+    z-index: 2;
 
     background: linear-gradient(90deg, rgba(255,149,0,1) 0%, rgba(189,29,29,1) 13%, rgba(208,0,249,1) 39%, rgba(69,160,148,1) 76%, rgba(0,212,255,1) 100%); 
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
 
-    @media screen and (max-width: 550px) {
+  @media screen and (max-width: 1200px) {
+    font-size: 5rem;
+  }
+
+  @media screen and (max-width: 800px) {
+    font-size: 4rem;
+  }
+
+  @media screen and (max-width: 450px) {
     font-size: 2.5rem;
   }
 
@@ -128,80 +150,43 @@ const Title = styled(motion.h1)`
     font-size: 2.1rem;
   }
 `
-
-const DiscContainer = styled.div`
+const DiscContainer = styled(motion.div)`
   position: relative;
-  width: 45%;
-  height: 90%;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(2, 1fr);
-  /* column-gap: 0.75rem; */
-  /* row-gap: 0.75rem; */
-
-  transition: all 1s;
+  display: flex;
+  width: 450px;
+  height: 450px;
+  flex-wrap: wrap;
+  z-index: 2;
+  /* transition: all 1s; */
   
-
-  /* @media screen and (max-width: 1770px) {
-    width: 55%;
-    height: 85%;
+   @media screen and (max-width: 2000px) {
+    width: 750px;
+    height: 750px;
   }
 
   @media screen and (max-width: 1600px) {
-    width: 40%;
-    height: 80%;
-  }
-
-  @media screen and (max-width: 1480px) {
-    width: 50%;
-    height: 70%;
-  }
-
-  @media screen and (max-width: 1360px) {
-    width: 47%;
-    height: 59%;
-  }
-
-  @media screen and (max-width: 1080px) {
-    width: 70%;
-    height: 67%;
-  }
-
-  @media screen and (max-width: 920px) {
-    width: 80%;
-    height: 60%;
+    width: 650px;
+    height: 650px;
   }
 
   @media screen and (max-width: 800px) {
-    width: 85%;
-    height: 65%;
+    width: 450px;
+    height: 450px;
   }
 
-  @media screen and (max-width: 750px) {
-    width: 85%;
-    height: 65%;
-  }
-
-
-  @media screen and (max-width: 550px) {
-    width: 97%;
-    height: 45%;
-  }
-
-  @media screen and (max-height: 700px){
-    height: 57%;
-  } */
+  @media screen and (max-width: 400px) {
+    width: 350px;
+    height: 350px;
+  } 
 `
 
 const Disc = styled(motion.div)`
   position: relative;
-  width: 100%;
-  height: 100%;
+  /* width: 100%;
+  height: 100%; */
   background-color: white;
-  
-
   &img{
-
+    object-fit: cover;
   }
 `
 
@@ -209,8 +194,8 @@ const FaderContainer = styled.div`
     width: 20%;
     position: absolute;
     left: 67%;
-
-    @media screen and (max-width: 1080px) {
+    z-index: 2;
+    @media screen and (max-width: 1400px) {
       position: absolute;
       width: 70%;
       top: 10%;
@@ -247,8 +232,20 @@ const Fader = styled.input`
   cursor: pointer;
 }
 
-  @media screen and (max-width: 1080px) {
+  @media screen and (max-width: 1400px) {
     transform: rotate(0deg);
   }
 
+`
+
+const Luz = styled.input`
+    position: absolute;
+    width: 600px;
+    height: 355px;
+    z-index: 1;
+    background: ${props => props.background || '#ff6afb'};
+    filter: blur(149px);
+    top: ${props => props.top || 0};
+    left: ${props => props.left || 0};
+    overflow: hidden
 `
